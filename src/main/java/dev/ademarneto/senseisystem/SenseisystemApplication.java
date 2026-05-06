b38efaf30381ef41bf878c325a5de59c9a4c6762
Ademar Neto <ademarnetodev@gmail.com>
2026-05-06 19:06:25 -0300

    first commit

---
+++ src/main/java/dev/ademarneto/senseisystem/SenseisystemApplication.java
@@ -0,0 +1,13 @@
+package dev.ademarneto.senseisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseisystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenseisystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ademarneto/senseisystem/SenseisystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ademarneto.senseisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenseisystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
